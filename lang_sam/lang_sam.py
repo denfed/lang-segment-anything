0b02dcd7c57819a82c951a7857f84aeed0de9182
denfed <dcfedori@buffalo.edu>
2024-11-13 16:43:49 -0500

    removed SAM

--- lang_sam/lang_sam.py
+++ lang_sam/lang_sam.py
@@ -12,11 +12,11 @@ from huggingface_hub import hf_hub_download
 from segment_anything import sam_model_registry
 from segment_anything import SamPredictor
 
-SAM_MODELS = {
-    "vit_h": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
-    "vit_l": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
-    "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth"
-}
+# SAM_MODELS = {
+#     "vit_h": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
+#     "vit_l": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
+#     "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth"
+# }
 
 CACHE_PATH = os.environ.get("TORCH_HOME", os.path.expanduser("~/.cache/torch/hub/checkpoints"))
 
@@ -53,33 +53,33 @@ class LangSAM():
         self.sam_type = sam_type
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.build_groundingdino()
-        self.build_sam(ckpt_path)
+        # self.build_sam(ckpt_path)
 
-    def build_sam(self, ckpt_path):
-        if self.sam_type is None or ckpt_path is None:
-            if self.sam_type is None:
-                print("No sam type indicated. Using vit_h by default.")
-                self.sam_type = "vit_h"
-            checkpoint_url = SAM_MODELS[self.sam_type]
-            try:
-                sam = sam_model_registry[self.sam_type]()
-                state_dict = torch.hub.load_state_dict_from_url(checkpoint_url)
-                sam.load_state_dict(state_dict, strict=True)
-            except:
-                raise ValueError(f"Problem loading SAM please make sure you have the right model type: {self.sam_type} \
-                    and a working checkpoint: {checkpoint_url}. Recommend deleting the checkpoint and \
-                    re-downloading it.")
-            sam.to(device=self.device)
-            self.sam = SamPredictor(sam)
-        else:
-            try:
-                sam = sam_model_registry[self.sam_type](ckpt_path)
-            except:
-                raise ValueError(f"Problem loading SAM. Your model type: {self.sam_type} \
-                should match your checkpoint path: {ckpt_path}. Recommend calling LangSAM \
-                using matching model type AND checkpoint path")
-            sam.to(device=self.device)
-            self.sam = SamPredictor(sam)
+    # def build_sam(self, ckpt_path):
+    #     if self.sam_type is None or ckpt_path is None:
+    #         if self.sam_type is None:
+    #             print("No sam type indicated. Using vit_h by default.")
+    #             self.sam_type = "vit_h"
+    #         checkpoint_url = SAM_MODELS[self.sam_type]
+    #         try:
+    #             sam = sam_model_registry[self.sam_type]()
+    #             state_dict = torch.hub.load_state_dict_from_url(checkpoint_url)
+    #             sam.load_state_dict(state_dict, strict=True)
+    #         except:
+    #             raise ValueError(f"Problem loading SAM please make sure you have the right model type: {self.sam_type} \
+    #                 and a working checkpoint: {checkpoint_url}. Recommend deleting the checkpoint and \
+    #                 re-downloading it.")
+    #         sam.to(device=self.device)
+    #         self.sam = SamPredictor(sam)
+    #     else:
+    #         try:
+    #             sam = sam_model_registry[self.sam_type](ckpt_path)
+    #         except:
+    #             raise ValueError(f"Problem loading SAM. Your model type: {self.sam_type} \
+    #             should match your checkpoint path: {ckpt_path}. Recommend calling LangSAM \
+    #             using matching model type AND checkpoint path")
+    #         sam.to(device=self.device)
+    #         self.sam = SamPredictor(sam)
 
     def build_groundingdino(self):
         ckpt_repo_id = "ShilongLiu/GroundingDINO"
@@ -100,22 +100,23 @@ class LangSAM():
 
         return boxes, logits, phrases
 
-    def predict_sam(self, image_pil, boxes):
-        image_array = np.asarray(image_pil)
-        self.sam.set_image(image_array)
-        transformed_boxes = self.sam.transform.apply_boxes_torch(boxes, image_array.shape[:2])
-        masks, _, _ = self.sam.predict_torch(
-            point_coords=None,
-            point_labels=None,
-            boxes=transformed_boxes.to(self.sam.device),
-            multimask_output=False,
-        )
-        return masks.cpu()
+    # def predict_sam(self, image_pil, boxes):
+    #     image_array = np.asarray(image_pil)
+    #     self.sam.set_image(image_array)
+    #     transformed_boxes = self.sam.transform.apply_boxes_torch(boxes, image_array.shape[:2])
+    #     masks, _, _ = self.sam.predict_torch(
+    #         point_coords=None,
+    #         point_labels=None,
+    #         boxes=transformed_boxes.to(self.sam.device),
+    #         multimask_output=False,
+    #     )
+    #     return masks.cpu()
 
-    def predict(self, image_pil, text_prompt, box_threshold=0.3, text_threshold=0.25):
-        boxes, logits, phrases = self.predict_dino(image_pil, text_prompt, box_threshold, text_threshold)
-        masks = torch.tensor([])
-        if len(boxes) > 0:
-            masks = self.predict_sam(image_pil, boxes)
-            masks = masks.squeeze(1)
-        return masks, boxes, phrases, logits
+    # def predict(self, image_pil, text_prompt, box_threshold=0.3, text_threshold=0.25):
+    #     boxes, logits, phrases = self.predict_dino(image_pil, text_prompt, box_threshold, text_threshold)
+    #     # masks = torch.tensor([])
+    #     # if len(boxes) > 0:
+    #     #     masks = self.predict_sam(image_pil, boxes)
+    #     #     masks = masks.squeeze(1)
+
+    #     return masks, boxes, phrases, logits
